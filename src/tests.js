1fb803dc1c4ac1bab3ab78cb4dcb3444d289b661
Viviane <viviane.a.gaspar@gmail.com>
2019-02-07 11:21:39 -0200

    renomeado readme

--- src/tests.js
+++ src/tests.js
@@ -1,20 +1,20 @@
 
-var resultado = cipher.encode(1,"abc");
+var resultado = cipher.encode(1, "abc");
 if (resultado === "bcd") {
   console.log("teste passou");
 } else {
   console.log('deu pau' + resultado + 'bcd')
 } 
 
-var resultado = cipher.encode(7,"abc");
+var resultado = cipher.encode(7, "abc");
 if (resultado === "hij") {
   console.log("teste passou");
 } else {
   console.log('deu pau' + resultado + 'hij')
 } 
 
-var resultado = cipher.encode(1,"ABC");
-if (resultado === "hij") {
+var resultado = cipher.encode(1, "ABC");
+if (resultado === "HIJ") {
   console.log("teste passou");
 } else {
   console.log('deu pau' + resultado + 'BCD')
